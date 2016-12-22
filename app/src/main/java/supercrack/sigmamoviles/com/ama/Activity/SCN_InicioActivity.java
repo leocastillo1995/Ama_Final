e56b198a6f8256da6935c273dfbd7a5b5724b683
eglp <EnriqueJose16>
2016-12-22 13:42:27 -0500

    completo las 3 primeras activitys solo faltan detalles

---
+++ app/src/main/java/supercrack/sigmamoviles/com/ama/Activity/SCN_InicioActivity.java
@@ -0,0 +1,25 @@
+package supercrack.sigmamoviles.com.ama.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import supercrack.sigmamoviles.com.ama.R;
+
+public class SCN_InicioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scn__inicio);
+
+        barra("Bienvenido");
+    }
+
+    private void barra(String titulo)
+    {
+        Toolbar toll = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toll);
+        getSupportActionBar().setTitle(titulo);
+    }
+}
--- app/src/main/java/supercrack/sigmamoviles/com/ama/SplashActivity.java
+++ app/src/main/java/supercrack/sigmamoviles/com/ama/SplashActivity.java
@@ -1,10 +1,11 @@
 package supercrack.sigmamoviles.com.ama;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -12,6 +13,7 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import supercrack.sigmamoviles.com.ama.Activity.CN_InicioActivity;
+import supercrack.sigmamoviles.com.ama.Activity.SCN_InicioActivity;
 import supercrack.sigmamoviles.com.ama.Conexion.ServicioAma;
 import supercrack.sigmamoviles.com.ama.Modelo.Token;
 import supercrack.sigmamoviles.com.ama.Utils.ElementosWebservis;
@@ -49,7 +51,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call<Token> call, Throwable t) {
 
-                Toast.makeText(SplashActivity.this , "ERROR" , Toast.LENGTH_SHORT).show();
+                mensaje("Ama a detectado que no está conectado se activara el modo cuestionario para que lo pueda utilizar. Cuando se vuelva conectar se sincronizara los datos registrados ¡Gracias por su atención!");
                 dialog.dismiss();
             }
         });
@@ -69,6 +71,35 @@ public class SplashActivity extends AppCompatActivity {
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
         startActivity(intent);
+    }
+
+    private void SCN_Inicio()
+    {
+        Intent intent = new Intent(this , SCN_InicioActivity.class);
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        startActivity(intent);
+    }
+
+    private void mensaje(String mensaje)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Ama");
+        builder.setMessage(mensaje);
+        builder.setNeutralButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                SCN_Inicio();
+
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
 }
